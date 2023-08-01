837e7e3cda1310bb9a32694290849183a0f0868e
steadysamwise4 <davenportsam44@gmail.com>
2023-08-01 12:46:56 -0500

    methods added to Terminal class, tested in example/main.dart

--- example/main.dart
+++ example/main.dart
@@ -1,3 +1,9 @@
+import 'package:dart_prompter_sd/src/terminal.dart';
+
 void main() {
-  print('Hi there!');
+  var terminal = Terminal();
+  terminal.clearScreen();
+  terminal.printPrompt('Hi there!');
+  var input = terminal.collectInput();
+  print('You just entered: $input');
 }
